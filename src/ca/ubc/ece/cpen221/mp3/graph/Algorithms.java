f6f1ae52706e37a0600dc2909643b4dfa41fe826
Sathish Gopalakrishnan <sathish.gopalakrishnan@gmail.com>
2015-10-13 01:46:16 -0700

    Source code skeletons and dataset

---
+++ src/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
@@ -0,0 +1,31 @@
+package ca.ubc.ece.cpen221.mp3.graph;
+
+import ca.ubc.ece.cpen221.mp3.staff.Graph;
+import ca.ubc.ece.cpen221.mp3.staff.Vertex;
+
+public class Algorithms {
+
+	/**
+	 * *********************** Algorithms ****************************
+	 * 
+	 * Please see the README for the machine problem for a more detailed
+	 * specification of the behavior of each method that one should implement.
+	 */
+
+	/**
+	 * This is provided as an example to indicate that this method and other
+	 * methods should be implemented here.
+	 * 
+	 * You should write the specs for this and all other methods.
+	 * 
+	 * @param graph
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static int shortestDistance(Graph graph, Vertex a, Vertex b) {
+		// TODO: Implement this method and others
+		return 0;
+	}
+
+}
