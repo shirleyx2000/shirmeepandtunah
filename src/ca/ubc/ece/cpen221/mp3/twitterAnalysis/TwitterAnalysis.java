01b6c3e07bfd561e3fffd2713200c033ef4a2aa4
Xueying Xie <shirleyx2000@gmail.com>
2015-10-26 07:04:50 -0700

    First draft of TwitterAnalysis

--- src/ca/ubc/ece/cpen221/mp3/twitterAnalysis/TwitterAnalysis.java
+++ src/ca/ubc/ece/cpen221/mp3/twitterAnalysis/TwitterAnalysis.java
@@ -1,6 +1,21 @@
 package ca.ubc.ece.cpen221.mp3.twitterAnalysis;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import ca.ubc.ece.cpen221.mp3.graph.AdjacencyListGraph;
+import ca.ubc.ece.cpen221.mp3.graph.AdjacencyMatrixGraph;
+import ca.ubc.ece.cpen221.mp3.graph.Algorithms;
+import ca.ubc.ece.cpen221.mp3.graph.PathNotFoundException;
+import ca.ubc.ece.cpen221.mp3.staff.Graph;
+import ca.ubc.ece.cpen221.mp3.staff.Vertex;
 
 /*
  * This class' scope is limited to the package. <-- remove this line before mp3 submission
@@ -8,17 +23,6 @@ import java.util.List;
  */
 
 class TwitterAnalysis{
-    
-    /*Implement the following methods:
-     * 
-     *   Type of queries:
-     *      commonInfluencers()
-     *      numRetweets()
-     *      
-     *   Query file processing:
-     *      main()
-     * 
-     */
     
     /**
      * Returns a list of common Twitter users that both user a and b follow
@@ -28,8 +32,13 @@ class TwitterAnalysis{
      * @return List of common Twitter users that both users a and b follow
      * 
      */
-    private static List<UserId> commonInfluencers( UserId a, UserId b ){
-        return null;
+    private static List<Vertex> commonInfluencers( Vertex a, Vertex b, Graph g ){
+        List<Vertex> ci = new ArrayList<Vertex>(); 
+        
+        //downstream neighbor of a and b are common users they follow
+        ci = Algorithms.commonDownstreamVertices(g, a, b);
+        
+        return Collections.unmodifiableList(ci);
     }
     
     /**
@@ -40,20 +49,140 @@ class TwitterAnalysis{
      * @return Number of retweets before a tweet by user a reaches user b
      * 
      */
-    private static int numRetweets( UserId a, UserId b ){
-        return 0;
+    private static int numRetweets( Vertex a, Vertex b, Graph g ){
+        int numRTW = 0; 
+        
+        try {
+            numRTW = Algorithms.shortestDistance(g, b, a);
+        } catch (PathNotFoundException pnfe) { }
+        
+        return numRTW;
     }
     
+    /**
+     * main() should take in two arguments:
+     * 
+     * 1. name of file that contains list of queries
+     * 2. name of file that should be used to write output of queries to
+     * 
+     * commonInfluencers and numRetweets should be called here during query file processing.
+     * 
+     */
     public static void main(String[ ] args){
-        /*
-         * main() should take in two arguments:
-         * 
-         * 1. name of file that contains list of queries
-         * 2. name of file that should be used to write output of queries to
-         * 
-         * commonInfluencers and numRetweets should be called here during query file processing.
-         * 
-         */
+        
+        //output file, ready to write
+        PrintWriter writer = null;
+        try {
+//            writer = new PrintWriter(args[1], "UTF-8");
+            writer = new PrintWriter("tweetResults.txt", "UTF-8");
+        } catch (FileNotFoundException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } catch (UnsupportedEncodingException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+        //other declarations 
+        Graph g = new AdjacencyMatrixGraph(); 
+        List<String> pastQueries = new ArrayList<String>();
+        List<Vertex> ciResults = new ArrayList<Vertex>();
+        int numRtwResults; 
+        
+        //form the graph to pass into two methods 
+        FileInputStream tweetStream;
+        try {
+            tweetStream = new FileInputStream("C:\\Users\\Shirley\\Documents\\Shirley2015\\CPEN221\\mp3-fall2015\\datasets\\twitter.txt");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        
+        try {
+            BufferedReader tweetReader = new BufferedReader(
+                    new InputStreamReader(tweetStream));
+            
+            String line;
+            int lineNum = 0; 
+            while ((line = tweetReader.readLine()) != null) {
+                System.out.println(lineNum);
+                //for each line, form the edges and add vertices
+                String[] vertices = line.split(" -> ");
+                Vertex v1 = new Vertex(vertices[0]);
+                Vertex v2 = new Vertex(vertices[1]);
+                g.addVertex(v1);
+                g.addVertex(v2);
+                g.addEdge(v1, v2);
+                
+                lineNum++; 
+            }
+            
+            tweetReader.close();
+            tweetStream.close();
+            
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        FileInputStream queryStream; 
+        try {
+//            queryStream = new FileInputStream(args[0]);
+            queryStream = new FileInputStream("queryTweets.txt");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        
+        try {
+            BufferedReader queryReader = new BufferedReader(
+                    new InputStreamReader(queryStream));
+            
+            String singleLine; 
+
+            while ((singleLine = queryReader.readLine()) != null) {
+                String[] queryWords = singleLine.split(" ");
+                //MUST end in '?'
+                if (!queryWords[queryWords.length-1].equals("?")) {
+                    continue; 
+                }
+                //Cannot accept any other format either 
+                else if (queryWords.length != 4 && 
+                   (!queryWords[0].equals("commonInfluencer") || !queryWords[0].equals("numRetweets"))){
+                    continue; 
+                } else {
+                    // check for duplicate query 
+                    if (pastQueries.contains(singleLine)) {
+                        continue; 
+                    } else {
+                        //ACTUALLY EXECUTE COMMAND HERE
+                        pastQueries.add(singleLine);
+                        
+                        if (singleLine.contains("commonInfluencer")){
+                            ciResults = commonInfluencers(new Vertex(queryWords[1]), new Vertex(queryWords[2]), g); 
+                            writer.println("query: " + singleLine);
+                            writer.println("<result>");
+                            for (Vertex v : ciResults) {
+                                writer.println(v.toString());
+                            }
+                            writer.println("</result>");
+                            writer.close();
+                        } else if (singleLine.contains("numRetweets")) {
+                            numRtwResults = numRetweets(new Vertex(queryWords[1]), new Vertex(queryWords[2]), g);
+                            writer.println("query: " + singleLine);
+                            writer.println("<result>");
+                            writer.println(numRtwResults);
+                            writer.println("</result>");
+                            writer.close();
+                        } else {
+                            continue; 
+                        }
+                    }
+                }
+            }
+            queryReader.close();
+            queryStream.close(); 
+            
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        
     }
-    
 }
