c9e29bfd1c7fc341f448ba56d1f641dcbb1b3a3b
tuanhle <guesswhotutu@gmail.com>
2015-10-25 20:00:27 -0700

    Added PathNotFoundException, tests for BFS and shortest path.

--- src/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
+++ src/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
@@ -25,15 +25,19 @@ public class Algorithms {
     * Finds shortest distance between two vertices a and b in an unweighted graph G. 
     * The shortest distance is the number of edges traversed to get to a from b.
     * Distance between a vertex and itself is 0. 
-    * If no such path exists from a to b, then TODO: [insert appropriate action]
+    * If no such path exists from a to b, then throw PathNotFoundException
+    * 
+    * Precondition: a and b must vertices on graph
+    * Postcondition: Smallest number of edges traversed from a to b returned
     * 
     * @param graph
     * @param a
     * @param b
     * @return Number of edges traversed to get from vertex a to b
-    *         if no path between a and b exists, TODO: [insert alt. return value]
+    *         if no path between a and b exists, a PathNotFoundException is thrown
+    * @throws PathNotFoundException
     */
-    public static int shortestDistance(Graph graph, Vertex a, Vertex b) {
+    public static int shortestDistance(Graph graph, Vertex a, Vertex b) throws PathNotFoundException {
        // TODO: Implement this method and others
        return 0;
     }
@@ -43,11 +47,12 @@ public class Algorithms {
 	/**
 	 * Traverses an entire graph
 	 * 
-	 * @param graph    Non-null graph to traverse
+	 * @param graph    Graph to traverse
 	 * 
 	 * @return bfs     An immutable set of traversal routines
 	 *                 Each traversal routine is a list of vertices in the order by which they were visited,
 	 *                 Each traversal starts from a vertex in the graph
+	 *                 If no traversal routines are found, an empty set is returned
 	 */
 	public static Set<List<Vertex>> breadthFirstSearch( Graph graph ){
 	    //TODO: Implement this method
@@ -83,11 +88,12 @@ public class Algorithms {
 	/**
 	 * Traverses an entire graph
 	 * 
-	 * @param graph     Non-null graph to traverse
+	 * @param graph     Graph to traverse
 	 * 
 	 * @return dfs      An immutable set of traversal routines
      *                  Each traversal routine is a list of vertices in the order by which they were visited
      *                  Each traversal starts from a vertex in the graph
+     *                  If no traversal routines are found, an empty set is returned
 	 */
 	public static Set<List<Vertex>> depthFirstSearch( Graph graph ){
 	  //TODO: Implement this method
