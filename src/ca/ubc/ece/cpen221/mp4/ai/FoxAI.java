67956b8ef03286bea039aaef9b03b598f7535a24
tuanhle <guesswhotutu@gmail.com>
2015-10-29 10:45:00 -0700

    Added mp4 project to repo

---
+++ src/ca/ubc/ece/cpen221/mp4/ai/FoxAI.java
@@ -0,0 +1,36 @@
+package ca.ubc.ece.cpen221.mp4.ai;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import ca.ubc.ece.cpen221.mp4.ArenaWorld;
+import ca.ubc.ece.cpen221.mp4.Direction;
+import ca.ubc.ece.cpen221.mp4.Location;
+import ca.ubc.ece.cpen221.mp4.Util;
+import ca.ubc.ece.cpen221.mp4.World;
+import ca.ubc.ece.cpen221.mp4.commands.BreedCommand;
+import ca.ubc.ece.cpen221.mp4.commands.Command;
+import ca.ubc.ece.cpen221.mp4.commands.EatCommand;
+import ca.ubc.ece.cpen221.mp4.commands.MoveCommand;
+import ca.ubc.ece.cpen221.mp4.commands.WaitCommand;
+import ca.ubc.ece.cpen221.mp4.items.Item;
+import ca.ubc.ece.cpen221.mp4.items.animals.*;
+
+/**
+ * Your Fox AI.
+ */
+public class FoxAI extends AbstractAI {
+	private int closest = 2; // max number; greater than fox's view range
+
+	public FoxAI() {
+
+	}
+
+	@Override
+	public Command getNextAction(ArenaWorld world, ArenaAnimal animal) {
+		// TODO: Change this. Implement your own AI to make decisions regarding
+		// the next action.
+		return new WaitCommand();
+	}
+
+}
---
+++ src/ca/ubc/ece/cpen221/mp4/ai/RabbitAI.java
@@ -0,0 +1,37 @@
+package ca.ubc.ece.cpen221.mp4.ai;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import ca.ubc.ece.cpen221.mp4.ArenaWorld;
+import ca.ubc.ece.cpen221.mp4.Direction;
+import ca.ubc.ece.cpen221.mp4.Location;
+import ca.ubc.ece.cpen221.mp4.Util;
+import ca.ubc.ece.cpen221.mp4.commands.BreedCommand;
+import ca.ubc.ece.cpen221.mp4.commands.Command;
+import ca.ubc.ece.cpen221.mp4.commands.EatCommand;
+import ca.ubc.ece.cpen221.mp4.commands.MoveCommand;
+import ca.ubc.ece.cpen221.mp4.commands.WaitCommand;
+import ca.ubc.ece.cpen221.mp4.items.Item;
+import ca.ubc.ece.cpen221.mp4.items.animals.ArenaAnimal;
+import ca.ubc.ece.cpen221.mp4.items.animals.Fox;
+import ca.ubc.ece.cpen221.mp4.items.animals.Rabbit;
+
+/**
+ * Your Rabbit AI.
+ */
+public class RabbitAI extends AbstractAI {
+
+	private int closest = 10; // max number; greater than rabbit's view range
+	private int temp;
+	private boolean foxFound;
+
+	public RabbitAI() {
+	}
+
+	@Override
+	public Command getNextAction(ArenaWorld world, ArenaAnimal animal) {
+		// TODO: Change this. Implement your own AI rules.
+		return new WaitCommand();
+	}
+}
