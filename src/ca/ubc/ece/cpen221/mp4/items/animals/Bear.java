eda4fb20544b6c47abff59de6ebceae649972757
Xueying Xie <shirleyx2000@gmail.com>
2015-11-07 14:25:53 -0800

    created bear animal

---
+++ src/ca/ubc/ece/cpen221/mp4/items/animals/Bear.java
@@ -0,0 +1,18 @@
+package ca.ubc.ece.cpen221.mp4.items.animals;
+
+import ca.ubc.ece.cpen221.mp4.items.LivingItem;
+
+public class Bear extends AbstractArenaAnimal {
+
+    @Override
+    public LivingItem breed() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "Bear"; 
+    }
+
+}
