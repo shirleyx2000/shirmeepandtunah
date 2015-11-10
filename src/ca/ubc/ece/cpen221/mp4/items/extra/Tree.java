804073d19f46c57ad8ba71947cb946576987bf36
Xueying Xie <shirleyx2000@gmail.com>
2015-11-09 19:41:06 -0800

    add tree

---
+++ src/ca/ubc/ece/cpen221/mp4/items/extra/Tree.java
@@ -0,0 +1,72 @@
+package ca.ubc.ece.cpen221.mp4.items.extra;
+
+import javax.swing.ImageIcon;
+
+import ca.ubc.ece.cpen221.mp4.Location;
+import ca.ubc.ece.cpen221.mp4.Util;
+import ca.ubc.ece.cpen221.mp4.items.Item;
+
+public class Tree implements Item {
+
+    private final static ImageIcon treeImage = Util.loadImage("tree.gif");
+
+    private Location location;
+    private boolean isDead;
+    
+    /**
+     * Places a Tree at <code> location </code>. The location must be valid and
+     * empty. Only way to die is through collision with car? 
+     *
+     * @param location
+     *            : the location where this tree will be created
+     */
+    public Tree(Location location) {
+        this.location = location;
+        this.isDead = false;
+    }
+    
+    @Override
+    public int getPlantCalories() {
+        //same as plants 
+        return 10;
+    }
+
+    @Override
+    public int getMeatCalories() {
+        // not a meat
+        return 0;
+    }
+
+    @Override
+    public ImageIcon getImage() {
+        return treeImage;
+    }
+
+    @Override
+    public String getName() {
+        return "tree";
+    }
+
+    @Override
+    public Location getLocation() {
+        return location;
+    }
+
+    @Override
+    public int getStrength() {
+        return 100;
+    }
+
+    @Override
+    public void loseEnergy(int energy) {
+        // tree shouldn't die because we have no planters
+        isDead = false; 
+    }
+
+    @Override
+    public boolean isDead() {
+        // groot shall not die!
+        return false;
+    }
+
+}
