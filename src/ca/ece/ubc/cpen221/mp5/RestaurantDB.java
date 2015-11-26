89d52b809f9b03afc79463d3806e1f95e241e371
Xueying Xie <shirleyx2000@gmail.com>
2015-11-25 22:33:16 -0800

    new method to generate db for restaurant, review, user

--- src/ca/ece/ubc/cpen221/mp5/RestaurantDB.java
+++ src/ca/ece/ubc/cpen221/mp5/RestaurantDB.java
@@ -6,13 +6,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-import javax.swing.text.html.HTMLDocument.Iterator;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -48,49 +47,62 @@ public class RestaurantDB {
 	public RestaurantDB(String restaurantJSONfilename, String reviewsJSONfilename, String usersJSONfilename) {
 		// TODO: Implement this method
 	    
-	    //Restaurant dealios 
-	    Map<String, RestaurantStruct> nomNoms = new HashMap<String, RestaurantStruct>(); 
-//	    Map<String, Map<String, String>> nomNoms = new HashMap<String, Map<String, String>>(); 
-	    Map<String, String> nomNomInfo = new HashMap<String, String>();
-	    List<String> nomFieldsStr = Arrays.asList("type", "business_id", "name", "neighbours", "full_address", 
-	            "city", "state", "photo_url", "categories", "open", "schools", "url", "longitude", "latitude", "stars", "review_count", "price"); 
-//	    List<String> nomFieldsInt = Arrays.asList("longitude", "latitude", "stars", "review_count", "price");
+	    Map json = generateDictionary(restaurantJSONfilename);
+        
+        System.out.println("\n\n\n");
+        Map json2 = generateDictionary(reviewsJSONfilename);
+        
+        System.out.println("\n\n\n");
+        Map json3 = generateDictionary(usersJSONfilename);
+        
+	}
+
+	/**
+	 * This helps the constructor generate a java map from the JSON file
+	 * 
+	 * @param file
+	 * @return json 
+	 */
+	public Map generateDictionary(String file) {
 	    
-	    //Review dealios 
-	    
-	    //User dealios
-	    
-	    //OPTION 1: EVALUATE EACH INDIVIDUAL LINE
-	    BufferedReader in;
+        BufferedReader in;
+        Map json = new HashMap(); 
         try {
             in = new BufferedReader(
-                    new FileReader("C:\\Users\\Shirley\\Documents\\Shirley2015\\CPEN221\\mp5-fall2015\\data\\" + restaurantJSONfilename));
+                    new FileReader("C:\\Users\\Shirley\\Documents\\Shirley2015\\CPEN221\\mp5-fall2015\\data\\" + file));
             String str; 
             
             try {
                 while ((str=in.readLine()) != null) {
-//                  System.out.println(str); 
-                  //str contains each line holding a restaurant
-                    Object obj = JSONValue.parse(str);
-                    JSONObject array = (JSONObject)obj; 
-                    String name = (String) array.get("name");
-                    System.out.println(name); 
-                    RestaurantStruct nomNomStruct = new RestaurantStruct((String) array.get("type"), 
-                                                                        (String) array.get("business_id"), 
-                                                                        (String) array.get("name"),
-                                                                        (String) array.get("full_address"),
-                                                                        (String) array.get("city"),
-                                                                        (String) array.get("state"),
-                                                                        (String) array.get("photo_url"),
-                                                                        (String) array.get("url"),
-                                                                        (double) array.get("longitude"), (double) array.get("latitude"), 
-                                                                        (double) array.get("stars"), (long) array.get("review_count"), 
-                                                                        (long) array.get("price"), (boolean) array.get("open"), 
-                                                                        (List<String>) array.get("categories"), 
-                                                                        (List<String>) array.get("neighbours"), (List<String>) array.get("schools")); 
-                    nomNoms.put(name, nomNomStruct);
+                    JSONParser parser = new JSONParser(); 
+                    ContainerFactory containerFactory = new ContainerFactory() {
+                        public List createArrayContainer() {
+                            return new LinkedList(); 
+                        }
+                        
+                        public Map createObjectContainer() {
+                            return new LinkedHashMap(); 
+                        }
 
-                    System.out.println(nomNoms);
+                      @Override
+                      public List creatArrayContainer() {
+                          // TODO Auto-generated method stub
+                          return null;
+                      }
+                    };
+                    
+                    try {
+                        json = (Map)parser.parse(str, containerFactory);
+                        Iterator iter = (Iterator) json.entrySet().iterator(); 
+                        while(iter.hasNext()) {
+                            Map.Entry entry = (Map.Entry)iter.next(); 
+                        }
+                        System.out.println(JSONValue.toJSONString(json));
+                        System.out.println(json.get("type"));
+                        
+                    } catch (ParseException pe) {
+                        System.out.println(pe);
+                    }
                       
                 }
             } catch (IOException e) {
@@ -102,17 +114,17 @@ public class RestaurantDB {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        
+        return json; 
 	}
-
+	
+	
 	//TO DELETE, testing only 
 	public static void main (String [] args) {
 	    RestaurantDB res = new RestaurantDB ("restaurants.json", "reviews.json", "users.json");
 	}
 	
 	/** 
-	 * 
-	 * 
-	 * 
 	 * @param queryString
 	 * @return Set<Restaurant> 
 	 */
--- src/ca/ece/ubc/cpen221/mp5/RestaurantStruct.java
+++ src/ca/ece/ubc/cpen221/mp5/RestaurantStruct.java
@@ -6,29 +6,49 @@ import java.util.List;
 
 public class RestaurantStruct {
     
+    public String type;
+    public String business_id; 
+    public String name; 
+    public String full_address; 
+    public String city; 
+    public String state; 
+    public String photo_url; 
+    public String url; 
+    
+    public double longitude; 
+    public double latitude; 
+    public double stars; 
+    public long review_count; 
+    public long price; 
+    
+    public boolean open; 
+    public List<String> categories; 
+    public List<String> neighbours; 
+    public List<String> schools;
+        
     public RestaurantStruct(String type, String business_id, String name,
             String full_address, String city, String state, String photo_url,
             String url, double longitude, double latitude, double stars, long review_count,
             long price, boolean open, List<String> categories, List<String> neighbours, List<String> schools) {
         
-        type = ""; 
-        business_id = ""; 
-        name = ""; 
-        full_address = ""; 
-        city = ""; 
-        state = ""; 
-        photo_url = ""; 
-        url = ""; 
+        this.type = type; 
+        this.business_id = business_id; 
+        this.name = name; 
+        this.full_address = full_address; 
+        this.city = city; 
+        this.state = state; 
+        this.photo_url = photo_url; 
+        this.url = url; 
         
-        longitude = 0.0; 
-        latitude = 0.0; 
-        stars = 0.0; 
-        review_count = 0; 
-        price = 0; 
+        this.longitude = longitude; 
+        this.latitude = latitude; 
+        this.stars = stars; 
+        this.review_count = review_count; 
+        this.price = price; 
         
-        open = false; 
-        categories = new ArrayList<String>(); 
-        neighbours = new ArrayList<String>(); 
-        schools = new ArrayList<String>(); 
+        this.open = open; 
+        this.categories = categories; 
+        this.neighbours = neighbours; 
+        this.schools = schools; 
     }
 }
