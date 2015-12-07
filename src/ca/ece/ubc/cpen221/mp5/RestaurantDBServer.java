498297599d9bb9573117cfc5d6ce8b8abd118cce
tuanhle <guesswhotutu@gmail.com>
2015-12-06 23:34:32 -0800

    Draft RDBS

--- src/ca/ece/ubc/cpen221/mp5/RestaurantDBServer.java
+++ src/ca/ece/ubc/cpen221/mp5/RestaurantDBServer.java
@@ -14,9 +14,14 @@ import java.util.Set;
  * Returns JSON formatted string to client if client requests or database updates are illegal/invalid/incorrect.
  * 
  * @author guess_000
+ * Referenced from https://github.com/CPEN-221/example15 SocialServer.java
+ * 
  */
 
 public class RestaurantDBServer {
+    
+    int port;
+    RestaurantDB rdb;
 
 	/**
 	 * Constructor
@@ -25,7 +30,6 @@ public class RestaurantDBServer {
 	 * @param filename1
 	 * @param filename2
 	 * @param filename3
-	 * @throws IOException 
 	 */
 	public RestaurantDBServer(int port, String filename1, String filename2, String filename3) throws IOException {
 
@@ -33,43 +37,78 @@ public class RestaurantDBServer {
 	    String reviewsDetailsFile = filename2;
 	    String usersDetailsFile = filename3;
 	    
-	    try( 
-	            ServerSocket rdbsSocket = new ServerSocket( port );
-	            Socket clientSocket = rdbsSocket.accept(); //Waits for client to connect, blocks other threads until successful
-	            //Set output stream to client 
-	            PrintWriter output = new PrintWriter( clientSocket.getOutputStream(), true ); //autoflush on every println, printf, format
-	            //Get input stream from client
-	            BufferedReader input = new BufferedReader( new InputStreamReader( clientSocket.getInputStream() ) );
-	    ){
-	        
-	        //Client connection successful here! 
-	        //Return output according to client query.
-	        
-	        String inputLine = input.toString();
-	        Set<Restaurant> outputSet = new HashSet<Restaurant>();
-	        
-	        //Create instance of RDB
-	        RestaurantDB rdb = new RestaurantDB( restaurantDetailsFile, reviewsDetailsFile, usersDetailsFile );
-	        //Get set of restaurants to return
-	        outputSet = rdb.query( inputLine );
-	        
-	        //Convert Set<Restaurant> to String in JSON format
-	        String outputString = "";
-	        for( Restaurant restaurant : outputSet ){
-	            outputString += convertRestaurantToJSON( restaurant );
-	        }
-	        //Return resulting string to client
-	        output.print( outputString );
+	    rdb = new RestaurantDB( restaurantDetailsFile, reviewsDetailsFile, usersDetailsFile );
+	    this.port = port;
+	    
+	}
+	
+	public void serve(){
+        
+        try{
+            
+            ServerSocket rdbsSocket = new ServerSocket( port );
+            
+            while( true ){
+                //Blocks other threads until a client connects
+                final Socket clientSocket = rdbsSocket.accept(); 
+                
+                //Start a new thread to handle connection
+                Thread thread = new Thread( new Runnable() {
+                    public void run(){
+                        //Thread belongs to current client socket; not touched again by main thread
+                        handle( clientSocket );
+                    }
+                });
+                
+                thread.start();
+                //Thread stops when client disconnects
+            }
 
+        } catch ( IOException ioe ){
+            System.out.println("Exception caught when trying to listen on port " + port + "or listening for a connection");
+            System.out.println( ioe.getMessage() );
+            return;
+        }
+	}
+	
+	/**
+	 * Handles a single client connection. Returns when client disconnects.
+	 * @param clientSocket
+	 * @throws IOException if connection has error or terminates unexpectedly
+	 */
+	public void handle( Socket clientSocket ) {
+	    
+	    try{
+	      //Set output stream to client 
+	        PrintWriter output = new PrintWriter( clientSocket.getOutputStream(), true ); //autoflush on every println, printf, format
+	        //Get input stream from client
+	        BufferedReader input = new BufferedReader( new InputStreamReader( clientSocket.getInputStream() ) );
+	    
+	        //input and output are thread-confined
 	        
+	        try{
+	          //Return output according to client query.
+	            String inputLine = input.toString();
+	            Set<Restaurant> outputSet = new HashSet<Restaurant>();
+	            
+	            //Get set of restaurants to return
+	            outputSet = rdb.query( inputLine );
+	            
+	            //Convert Set<Restaurant> to String in JSON format
+	            String outputString = "";
+	            for( Restaurant restaurant : outputSet ){
+	                outputString += convertRestaurantToJSON( restaurant );
+	            }
+	            //Return resulting string to client; autoflush on
+	            output.println( outputString ); 
+	        } finally {
+	            output.close();
+	            input.close();
+	            clientSocket.close();
+	        }
 	    } catch ( IOException ioe ){
-	        System.out.println("Exception caught when trying to listen on port " + port + "or listening for a connection");
-	        System.out.println( ioe.getMessage() );
+	        ioe.printStackTrace();
 	    }
-	    
-	    
-	    //TODO: Make TestClient class
-	    
 	}
 	
 	public String randomReview( String restaurantName ){
@@ -94,38 +133,6 @@ public class RestaurantDBServer {
 	    //add review to RestaurantDB object
 	}
 	
-	//TODO: PARSING METHODS; discuss with Shirley how to implement these methods
-	
-	//Converts a Restaurant into a JSON formatted string
-	private String convertRestaurantToJSON( Restaurant restaurant ){
-	    return null;
-	}
-	
-	//Converts a Review into a JSON formatted string
-    private String convertReviewToJSON( Review review ){
-        return null;
-    }
-    
-    //Converts a User into a JSON formatted string
-    private String convertUserToJSON( User user ){
-        return null;
-    }
-	
-	//Converts a JSON formatted string that represents a Restaurant into its Java object form
-	private Restaurant convertToRestaurant( String restaurantJSON ){
-	    return null;
-	}
-	
-	//Converts a JSON formatted string that represents a Review into its Java object form
-    private Review convertToReview( String reviewJSON ){
-        return null;
-    }
-    
-    //Converts a JSON formatted string that represents a User into its Java object form
-    private User convertToUser( String userJSON ){
-        return null;
-    }
-	
     //MAIN
     
 //	public static void main( String[] args ) throws ReservedPortException, IOException{
@@ -145,16 +152,14 @@ public class RestaurantDBServer {
 //	    String restaurantDetailsFile = args[1];
 //        String reviewsDetailsFile = args[2];
 //        String usersDetailsFile = args[4];
-//        boolean listening = true;
 //	    
 //	    //Don't forget to check for reserved (well-known) port numbers
 //	    if( port >=0 && port <= 1023 ){
 //	        throw new ReservedPortException("Using well-known port " + port);
 //	    }
 //	    
-//	    //Create instance of RDBS using args values, loop infinitely
+//	    //Create instance of RDBS, returns only if IOException
 //	    RestaurantDBServer rdbs = new RestaurantDBServer( port, restaurantDetailsFile, reviewsDetailsFile, usersDetailsFile );
-//	    //while( listening );
 //	    
 //	}
 
